7b6342b34e04bf225e272a04eae80fab4e775816
JoaolimaDev <ozymandiasphp@gmail.com>
2024-09-17 15:10:32 -0300

    Primeiro commit

---
+++ src/main/java/me/dio/formacao_java_cognizant/FormacaoJavaCognizantApplication.java
@@ -0,0 +1,13 @@
+package me.dio.formacao_java_cognizant;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FormacaoJavaCognizantApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FormacaoJavaCognizantApplication.class, args);
+	}
+
+}
